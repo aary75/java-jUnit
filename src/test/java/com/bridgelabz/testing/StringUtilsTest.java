a76f00db5ec769cb75ad7fbefb4de96ee8f04227
Aaryan <aaryankgarg@gmail.com>
2025-04-02 21:37:28 +0530

    [Aaryan] Checking the methods of string utils

--- src/test/java/com/bridgelabz/testing/StringUtilsTest.java
+++ src/test/java/com/bridgelabz/testing/StringUtilsTest.java
@@ -1,4 +1,35 @@
+// Checking all functions of method utils
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilsTest {
+
+    @Test
+    public void reverseTest(){
+        String expected = "nayraA";
+
+        Assert.assertEquals(expected, StringUtils.reverse("Aaryan"));
+    }
+
+    @Test
+    public void isPalindrome(){
+        boolean expected = false;
+
+        Assert.assertEquals(expected, StringUtils.isPalindrome("Aaryan"));
+    }
+
+    @Test
+    public void toUpperCase(){
+        String expected = "AARYAN";
+
+        Assert.assertEquals(expected, StringUtils.toUpperCase("Aaryan"));
+    }
 }
+
+//if by change 3rd test case does not pass, then output will be
+//org.junit.ComparisonFailure:
+//Expected :AArYAN
+//Actual   :AARYAN
