ab89cb66d893587ce49d0ac1ab9a95b90bf34427
Aaryan <aaryankgarg@gmail.com>
2025-04-03 23:16:11 +0530

    [Aaryan] checking that the temperature is changing perfectly or not

--- src/test/java/com/bridgelabz/testing/TemperatureChangeTest.java
+++ src/test/java/com/bridgelabz/testing/TemperatureChangeTest.java
@@ -1,4 +1,24 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.TemperatureChange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TemperatureChangeTest {
+
+    @Test
+    public void changeToFarheinheit(){
+        double result = TemperatureChange.celciusToFarhenheit(32);
+        double expected = 89.6;
+
+        Assertions.assertEquals(result, expected);
+    }
+
+    @Test
+    public void changeToCelcius(){
+        double result = TemperatureChange.farhenheitToCelcius(60);
+        double expected = 15.555555555555557;
+
+        Assertions.assertEquals(result,expected);
+    }
 }
