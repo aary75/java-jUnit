9ed96b629b86119cd61963446292bdfe723e18aa
Aaryan <aaryankgarg@gmail.com>
2025-04-03 14:41:07 +0530

    [Aaryan] Testing the isEven program

--- src/test/java/com/bridgelabz/testing/isEvenTest.java
+++ src/test/java/com/bridgelabz/testing/isEvenTest.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.isEven;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class isEvenTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {2, 4, 6, 7, 9})
+    public void isEven(int number){
+        Assertions.assertTrue(isEven.isEven(number));
+    }
 }
+
+//for odd number
+//
+//org.opentest4j.AssertionFailedError:
+//Expected :true
+//Actual   :false
+
