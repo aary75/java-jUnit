bafb2a7f768e3647efa600256a67c32ee14f5350
Aaryan <aaryankgarg@gmail.com>
2025-04-03 13:41:34 +0530

    [Aaryan] Checking that database is connected or not

--- src/test/java/com/bridgelabz/testing/DatabaseConnectionTest.java
+++ src/test/java/com/bridgelabz/testing/DatabaseConnectionTest.java
@@ -1,4 +1,38 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.DatabaseConnection;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DatabaseConnectionTest {
+
+    @BeforeEach
+    public void connected(){
+        DatabaseConnection.connect();
+    }
+
+    @Test
+    @Order(1)
+    public void test(){
+        Assertions.assertTrue(DatabaseConnection.isConnected());
+    }
+
+    @Test
+    @Order(2)
+    public void testing(){
+        Assertions.assertTrue(DatabaseConnection.isConnected());
+    }
+
+    @AfterEach
+    public void disconnected(){
+
+        DatabaseConnection.disconnect();
+        System.out.println();
+    }
 }
+
+//Database connected
+//Database disconnected
+//
+//Database connected
+//Database disconnected
