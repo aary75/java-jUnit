fab5a2f10bca7d4a4266731ecc9cc4cb383b6e75
Aaryan <aaryankgarg@gmail.com>
2025-04-03 23:04:01 +0530

    [Aaryan] Checking the given password is valid or not

--- src/test/java/com/bridgelabz/testing/PasswordStrengthValidatorTest.java
+++ src/test/java/com/bridgelabz/testing/PasswordStrengthValidatorTest.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.PasswordStrengthValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
 public class PasswordStrengthValidatorTest {
+
+    @Test
+    public void passwordChecking(){
+        String regex = "(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}";
+        String result = PasswordStrengthValidator.checking("Aary3cec", Pattern.compile(regex));
+
+        String expected = "Valid";
+        Assertions.assertEquals(result, expected);
+    }
 }
