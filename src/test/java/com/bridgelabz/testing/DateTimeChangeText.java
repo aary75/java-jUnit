cc3164d6984766de4557c32ce7d52a2ec87b7125
Aaryan <aaryankgarg@gmail.com>
2025-04-04 10:14:39 +0530

    [Aaryan] Checking the date changing format

--- src/test/java/com/bridgelabz/testing/DateTimeChangeText.java
+++ src/test/java/com/bridgelabz/testing/DateTimeChangeText.java
@@ -1,4 +1,29 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.DateTimeChange;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
 public class DateTimeChangeText {
+
+    @Test
+    public void DateTimeChange(){
+        String result = DateTimeChange.DateTimeFormatting("2024-03-21");
+        String expected = "21-03-2024";
+
+        Assertions.assertEquals(result,expected);
+
+    }
+
+    @Test
+    public void DateTimeChange1(){
+        String result = DateTimeChange.DateTimeFormatting("2024-03-21");
+        String expected = "28-03-2024";
+
+        Assertions.assertEquals(result,expected);
+
+    }
+
 }
