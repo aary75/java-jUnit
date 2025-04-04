fb12ea33e627c652803af735eeada4469104bbb0
Aaryan <aaryankgarg@gmail.com>
2025-04-04 11:26:38 +0530

    [Aaryan] Checking the  userName, userEmail and userPassword

--- src/test/java/com/bridgelabz/testing/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/testing/UserRegistrationTest.java
@@ -1,4 +1,21 @@
 package com.bridgelabz.testing;
 
+import com.bridgelabz.javaJunit.UserRegistration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserRegistrationTest {
+
+    @Test
+    public void Validation(){
+        boolean result = UserRegistration.register("Aaryancew","ryn@gmail.com","Aaryan2n");
+        boolean expected = true;
+        Assertions.assertEquals(result,expected);
+    }
+
+    @Test
+    public void invalid(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> UserRegistration.register("Aaryan","ryan@gmail.com","Aercecews"));
+
+    }
 }
